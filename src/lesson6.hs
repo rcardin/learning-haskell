02762cf5e4c0f7f708d3358190d6f1ded2f0b523
rcardin <riccardo.cardin@gmail.com>
2018-12-26 10:34:24 +0100

    Added examples of $ operator and functions composition

--- src/lesson6.hs
+++ src/lesson6.hs
@@ -34,12 +34,39 @@ quicksort' (x::xs)
     in smallerSorted ++ [x] ++ [greaterSorted]
 :}
 
+:{
 largestDivisible :: (Integral a) => a
 largestDivisible = head (filter p [100000, 99999..])
     where p x = x `mod` 3829 == 0
+:}
 
+:{
 sum' :: (Num a) => [a] -> a
 sum' xs = foldl (\acc x -> acc + x) 0 xs
+:}
 
+:{
 elem' :: (Eq a) => a -> [a] -> Bool
 elem' x xs = foldl (\acc y -> if y == x then True else acc ) False xs
+:}
+
+-- Because $ is right-associative, f (g (z x)) is equal to f $ g $ z x
+-- Use the $ operator to rewrite the following functions' composition
+-- sum (filter (> 10) (map (*2) [2..10]))
+sum $ filter (> 10) $ map (*2) [2..10]
+-- sum (map sqrt [1..130]) becomes
+sum $ map sqrt [1..130]
+
+-- (f ° g)(x) = f(g(x))
+-- Using function composition, map (\x -> negate (abs x)) [5,-3,-6,7,-3,2,-19,24] becomes
+map (negate . abs x) [5,-3,-6,7,-3,2,-19,24]
+-- map (\xs -> negate (sum (tail xs))) [[1..5],[3..6],[1..7]] becomes
+map (negate . sum . tail xs) [[1..5],[3..6],[1..7]]
+-- Composition does not work well with function having more than one parameter. We need 
+-- to use currying. So, sum (replicate 5 (max 6.7 8.9)) becomes
+(sum . replicate 5 . max 6.7) 8.9
+--  Or, using the $ operator
+sum . replicate 5 . max 6.7 $ 8.9
+-- replicate 100 (product (map (*3) (zipWith max [1,2,3,4,5] [4,5,6,7,8]))) becomes
+replicate 100 . product . map (*3) . zipWith max [1,2,3,4,5] $ [4,5,6,7,8]
+-- Are we sure that the last form is really more readable?
